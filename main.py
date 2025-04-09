3073863f18327713a07ead78dcaacb165717c381
Saeho Oh <saeho973@gmail.com>
2025-04-09 18:19:58 +0900

    fix: main.py와 requirements.txt 수정

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from supabase import create_client
 import os
 from dotenv import load_dotenv
+import uvicorn
 
 load_dotenv()
 
@@ -17,3 +18,6 @@ def read_root():
     # Supabase에서 "travelogue" 라는 테이블의 모든 데이터 조회
     data = supabase.table("travelogue").select("*").execute()
     return data.data
+
+if __name__ == "__main__":
+     uvicorn.run(app, host="0.0.0.0", port=8000)
