b3cc3c4a615e589163cf491daf57c24ba2bd62f7
sangmin <ss721229@naver.com>
2025-04-25 14:33:56 +0900

    chore: 독립된 파일에서 동시에 API 작업 가능하도록 Router 활용 #9

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 import uvicorn
-from sqlalchemy.orm import Session
-from models import Travelogue
-from database import SessionLocal
+from api_sm import router as router_sm
+from api_sh import router as router_sh
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.include_router(router_sm)
+app.include_router(router_sh)
 
-from pydantic import BaseModel
-from typing import List
-from datetime import datetime
-class TravelogueResponse(BaseModel):
-    id: int
-    style_category: int | None = None
-    created_at: datetime
-
-@app.get("/", response_model=List[TravelogueResponse])
-def get_test_with_db(db: Session = Depends(get_db)):
-    return db.query(Travelogue).all()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
