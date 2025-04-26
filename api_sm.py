51758832a5c30b88e7a820b9042c277d2ba59575
sangmin <ss721229@naver.com>
2025-04-26 11:31:37 +0900

    feat: 여행기 튜플 확인 (GET) API 추가 - 모든 튜플, 특정 id 튜플 #10

---
+++ api_sm.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel, Field
+from datetime import datetime
+from database import SessionLocal
+from typing import Annotated, List
+from sqlalchemy.orm import Session
+from models import Travelogue
+from starlette import status
+
+router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class TravelogueUpdate(BaseModel):
+    style_category: int = Field(ge=1, le=3)
+
+class TravelogueResponse(BaseModel):
+    id: int
+    style_category: int | None = None 
+    created_at: datetime
+
+
+@router.get(
+    "/api/travelogue/all",
+    status_code=status.HTTP_200_OK,
+    response_model=List[TravelogueResponse],
+    summary="모든 여행기 튜플 확인",
+    description="현재 데이터베이스에 저장된 모든 여행기 튜플을 확인합니다"
+)
+async def get_travelogue(db: db_dependency):
+    return db.query(Travelogue).all()
+
+
+@router.get(
+    "/api/travelogue/{travelogue_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=TravelogueResponse,
+    summary="특정 id 여행기 튜플 확인",
+    description="현재 데이터베이스에 저장된 특정 id 여행기 튜플을 확인합니다"
+)
+async def get_travelogue(travelogue_id: int, db: db_dependency):
+    db_travelogue = db.query(Travelogue).filter(Travelogue.id == travelogue_id).first()
+    if not db_travelogue:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={"error": "Travelogue not found"})
+    return db_travelogue
